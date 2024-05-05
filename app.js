83aa7859ccdcc6f9f4d05c993c3363bad4226319
Mirza Sarim Baig <sarimbaig321@gmail.com>
2024-05-06 00:23:58 +0500

    CRUD apis created

--- app.js
+++ app.js
@@ -1,57 +1,71 @@
 import express from "express";
 const app = express();
 const PORT = process.env.PORT || 5000;
-import mongoose from "mongoose";
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose
-  .connect(UIR)
-  .then((res) => {
-    console.log("Connection successful");
-  })
-  .catch((err) => {
-    console.log(" error", err.message);
-  });
-
-app.post("/api/data", (req, res) => {
-  console.log(req.body);
-  res.json({
-    message: "Record added successfully",
-  });
+app.post("/api/postdata", (req, res) => {
+  try {
+    console.log(req.body);
+    res.json({
+      message: "Record added successfully",
+      record: req.body,
+    });
+  } catch (error) {
+    res.json({
+      message: "error while adding Record",
+      data: [],
+      error: error.message,
+      status: false,
+    });
+  }
 });
 
-const mongoSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    // required: true,
-  },
-  email: {
-    // required: true,
-  },
+app.get("/api/getdata", (req, res) => {
+  try {
+    res.json({
+      message: "Data Fetched successfully",
+    });
+  } catch (error) {
+    res.json({
+      message: "error while fetching Record",
+      data: [],
+      error: error.message,
+      status: false,
+    });
+  }
 });
-const collectionData = new mongoose.model("sarim", mongoSchema);
 
-const data = [
-  {
-    name: "Sarim",
-    email: "sarim@gmail.com",
-    company: "Designers Rockwall",
-  },
-  {
-    name: "Mirza",
-    email: "mirza@gmail.com",
-    company: "mirza's cafe",
-  },
-  {
-    name: "Baig",
-    email: "baig@gmail.com",
-    company: "Baig Tower",
-  },
-];
+app.put("/api/updatedata", (req, res) => {
+  try {
+    res.json({
+      message: "Data Updated successfully",
+    });
+  } catch (error) {
+    res.json({
+      message: "error while updating Record",
+      data: [],
+      error: error.message,
+      status: false,
+    });
+  }
+});
 
-collectionData.insertMany(data);
+app.delete("/api/deletedata", (req, res) => {
+  try {
+    res.json({
+      message: "Data Deleted successfully",
+    });
+  } catch (error) {
+    res.json({
+      message: "error while deleting Record",
+      data: [],
+      error: error.message,
+      status: false,
+    });
+  }
+});
 
 app.listen(PORT, () => {
   console.log("Server running on port 5000");
